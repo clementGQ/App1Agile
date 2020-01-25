257d0321e6d293915d7ce565527a3f4c3b966fdf
ThomasDlp <thomas.delpy@wanadoo.fr>
2020-01-25 11:39:20 -0500

    Première version de l'interface graphique du projet

---
+++ xDraw4US/src/main/java/controller/MainApp.java
@@ -0,0 +1,83 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("xDraw4US");
+
+        initRootLayout();
+
+        showPalettes();
+    }
+    
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("../view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showPalettes() {
+        try {
+            // Load person overview.
+            FXMLLoader loader1 = new FXMLLoader();
+            loader1.setLocation(MainApp.class.getResource("../view/PaletteLayout.fxml"));
+            BorderPane paletteLayout = (BorderPane) loader1.load();
+            
+            FXMLLoader loader2 = new FXMLLoader();
+            loader2.setLocation(MainApp.class.getResource("../view/HorizontalPalette.fxml"));
+            AnchorPane horizontalPalette = (AnchorPane) loader2.load();
+            
+            FXMLLoader loader3 = new FXMLLoader();
+            loader3.setLocation(MainApp.class.getResource("../view/VerticalPalette.fxml"));
+            AnchorPane verticalPalette = (AnchorPane) loader3.load();
+            
+            rootLayout.setCenter(paletteLayout);
+            paletteLayout.setTop(horizontalPalette);
+            paletteLayout.setLeft(verticalPalette);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
