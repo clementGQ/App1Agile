1935aec44d83a3ce3aa10184cce18e5356df548f
ThomasDlp <thomas.delpy@wanadoo.fr>
2020-01-26 12:54:08 -0500

    Ajout bouton menus déroulant sélection pattern remplissage.
    Ajout des boutons, fonctions vides reliées aux boutons, il faut ajouter les corps des fonctions pour changer le mode de remplissage des figures.

--- xDraw4US/src/main/java/views/HorizontalBarController.java
+++ xDraw4US/src/main/java/views/HorizontalBarController.java
@@ -4,6 +4,8 @@ package views;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 
 public class HorizontalBarController extends AController{
@@ -13,6 +15,18 @@ public class HorizontalBarController extends AController{
 	
 	@FXML
 	private ColorPicker colorPicker;
+	
+	@FXML
+	private MenuButton fillingPatternMenuButton;
+	
+	@FXML
+	private MenuItem fillingPattern1;
+	
+	@FXML
+	private MenuItem fillingPattern2;
+	
+	@FXML
+	private MenuItem fillingPattern3;
 	
 	public HorizontalBarController() {
 		
@@ -39,4 +53,30 @@ public class HorizontalBarController extends AController{
         	minOrMaxButton.setText("800x600");
     	}
     }
+    
+    @FXML
+    public void patternFillingSelection() {
+    	System.out.println("Click on pattern filling button");    	
+    }
+    
+    @FXML
+    public void fillingPattern1Selection() {
+    	System.out.println("filling pattern 1");
+    	fillingPatternMenuButton.setText("coockies");
+    }
+    
+    @FXML
+    public void fillingPattern2Selection() {
+    	System.out.println("filling pattern 2");
+    	fillingPatternMenuButton.setText("option 2");
+    }
+    
+    @FXML
+    public void fillingPattern3Selection() {
+    	System.out.println("filling pattern 3");
+    	fillingPatternMenuButton.setText("option 3");
+    }
+    
+    
+    
 }
